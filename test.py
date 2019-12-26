13b034b0c110675377aa989b6524a6c1c145652e
tim <541218251@qq.com>
2019-12-26 14:01:00 +0800

    fix a bug

--- test.py
+++ test.py
@@ -1,2 +1,38 @@
-mat = "{:^4}\t{:^8}\t{:^12}"
-print(mat.format("占4个长度11", "占8个长度", "占12长度"))
+'''
+import cv2 as cv
+
+
+def video_demo():
+    #0是代表摄像头编号，只有一个的话默认为0
+    capture = cv.VideoCapture(0)
+    while (True):
+        ref, frame = capture.read()
+
+        cv.imshow("1", frame)
+        #等待30ms显示图像，若过程中按“Esc”退出
+        c = cv.waitKey(30) & 0xff
+        if c == 27:
+            capture.release()
+            break
+
+
+video_demo()
+cv.waitKey()
+cv.destroyAllWindows()
+'''
+import tensorflow as tf
+
+# Simple hello world using TensorFlow
+
+# Create a Constant op
+# The op is added as a node to the default graph.
+#
+# The value returned by the constructor represents the output
+# of the Constant op.
+hello = tf.constant('Hello, TensorFlow!')
+
+# Start tf session
+sess = tf.compat.v1.Session()
+
+# Run the op
+#print(sess.run(hello))
